52a34161e3eedd0219455c88f5c769fae5bd60c9
Nazar <lnazar2013@gmail.com>
2023-10-11 13:12:23 +0300

    New feature added

--- calc.cpp
+++ calc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ int main() {
 
   cout << "Введіть перше число\n";   cin >> a;
 
-  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";  cin >> op;
+  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n5\t**\n\n";  cin >> op;
 
   cout << "Введіть друге число\n";     cin >> b;
 
@@ -28,6 +29,10 @@ int main() {
                     break;
 
     case 4:     result = (double) a / b;
+
+                    break;
+
+    case 5:     result = pow(a,b);
 
                     break;
 
